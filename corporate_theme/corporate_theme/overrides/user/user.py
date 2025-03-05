64a9f03bdf989babf3774ddc6c26fc753eec7ec5
Farhan Hakim <farhanhakimdariman@gmail.com>
2025-03-05 12:41:03 +0800

    Fixed API request failed for theme switcher

--- corporate_theme/corporate_theme/overrides/user/user.py
+++ corporate_theme/corporate_theme/overrides/user/user.py
@@ -2,5 +2,5 @@ import frappe
 
 @frappe.whitelist()
 def switch_theme(theme):
-	if theme in ["Dark", "Light", "Automatic", "Corporate Theme"]:
+	if theme in ["Dark", "Light", "Automatic", "corporate_theme"]:
 		frappe.db.set_value("User", frappe.session.user, "desk_theme", theme)

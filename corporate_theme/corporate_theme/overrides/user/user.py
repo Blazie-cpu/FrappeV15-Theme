0f2809d809b45278a03c9219d2a9a3d9628bb2f1
Farhan Hakim <farhanhakimdariman@gmail.com>
2025-03-05 15:12:01 +0800

    Attemp to fix set user.theme db error

--- corporate_theme/corporate_theme/overrides/user/user.py
+++ corporate_theme/corporate_theme/overrides/user/user.py
@@ -1,16 +1,16 @@
-import frappe
+# import frappe
 
 # @frappe.whitelist()
 # def switch_theme(theme):
 # 	if theme in ["Dark", "Light", "Automatic", "corporate_theme"]:
 # 		frappe.db.set_value("User", frappe.session.user, "desk_theme", theme)
 #
-#Testing debugging code below, original code above.
+#Testing debugging code below,
 import frappe
 
 @frappe.whitelist()
 def switch_theme(theme):
-    if theme in ["Dark", "Light", "Automatic", "corporate_theme"]:
+    if theme in ["Dark", "Light", "Automatic", "Corporate Theme"]:
         frappe.db.set_value("User", frappe.session.user, "desk_theme", theme)
         frappe.msgprint(f"Theme switched to {theme} successfully!")
     else:

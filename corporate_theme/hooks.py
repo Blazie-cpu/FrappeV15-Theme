6ccd268252c0d0a37553d8d0e168c4b87ca16e7b
Blazie-cpu <58299672+Blazie-cpu@users.noreply.github.com>
2025-03-06 10:38:45 +0800

    Update hooks.py

--- corporate_theme/hooks.py
+++ corporate_theme/hooks.py
@@ -12,6 +12,10 @@ app_license = "mit"
 # include js, css files in header of desk.html
 app_include_js = ["corporate_theme.bundle.js"]
 app_include_css = "corporate_theme.bundle.css"
+app_include_js = ["red-hue-theme.bundle.js"]
+app_include_css = "red-hue-theme.bundle.css"
+
+
 
 # include js, css files in header of web template
 # web_include_css = "/assets/corporate_themes/css/corporate.css"
